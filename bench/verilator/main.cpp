673fdefa6ce82f157ac5112d1ea27030b0a7ce12
Bjorn Schouteten <b_schouteten@hotmail.com>
2023-05-02 21:51:28 +0200

    Add submodule Update Makefile and add makefile Update testbench files

---
+++ bench/verilator/main.cpp
@@ -0,0 +1,38 @@
+/**
+ * @file main.cpp
+ * @author B.Schouteten
+ * @brief 
+ * @version 0.1
+ * @date 2023-04-30
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include "tb_apb_uart16550.hpp"
+
+//Legacy function required only so linking works on Cygwin and MSVC++ and MacOS
+double sc_time_stamp() { return 0; }
+
+int main(int argc, char **argv)
+{
+    const std::unique_ptr<VerilatedContext> contextp(new VerilatedContext);
+
+    //Pass arguments to Verilated code
+    contextp->commandArgs(argc, argv);
+
+    //Create model for DUT
+    cAPBUart16550TestBench* testBench = new cAPBUart16550TestBench(contextp.get());
+
+    testBench->opentrace("waveform.vcd");
+
+    //Simulate the design (until $finish)
+    while (!testBench->finished())
+    {
+
+    }
+
+    delete testBench;
+    
+    //Completed succesfully
+    return 0;
+}
