10dacc27cd8cce52abd6e435bf5e7a774e6b5697
Bjorn Schouteten <b_schouteten@hotmail.com>
2023-09-09 22:05:23 +0200

    Update main and makefile Update on submodule

--- bench/verilator/main.cpp
+++ bench/verilator/main.cpp
@@ -9,29 +9,65 @@
  * 
  */
 #include <tb_apb_uart16550.hpp>
-#include <programoptions.hpp>
+//#include <programoptions.hpp>
+#include <valueOption.hpp>
+#include <noValueOption.hpp>
 
 using namespace RoaLogic::common;
 
 //Legacy function required only so linking works on Cygwin and MSVC++ and MacOS
 double sc_time_stamp() { return 0; }
 
+
+
+
 int main(int argc, char* argv[])
 {
     cProgramOptions programOptions;    
     const std::unique_ptr<VerilatedContext> contextp(new VerilatedContext);
 
-    programOptions.add("-l", "-log", "Log file path, when not specified log is written to terminal", true);
+    cNoValueOption helpOption("h", "help", "Show this help and exit", false);
+    cValueOption<std::string> logOption("l", "log", "Log file path, when not specified log is written to terminal");
+    cValueOption<bool> boolOption("b", "bool", "Test for boolean, defaults to false. Option could be '1', true, True or TRUE");
+    cValueOption<uint8_t> logPriorityOption("p", "priority", "Log priority. Debug = 0, Log = 1, Info = 2, Warning = 3, Error = 4, Fatal = 5");
+    uint8_t logPriority = 0;
+    
+    programOptions.add(&helpOption);
+    programOptions.add(&logOption);
+    programOptions.add(&logPriorityOption);
+    programOptions.add(&boolOption);
+    
+    programOptions.parse(argc, argv);
 
-    if(programOptions.parse(argc, argv) > 0)
+    if(helpOption.isSet())
     {
-        
+        programOptions.printKnownOptions();
+        return 0;
     }
 
-    cLog::init(eLogPriority::Info, "");
+    if (logPriorityOption.isSet())
+    {
+        logPriority = logPriorityOption.value();
+    }
+    else
+    {
+        logPriority = 2;
+    }    
 
-    cLog::info("Gone through init");
+    if(logOption.isSet())
+    {
+        cLog::getInstance()->init(logPriority, logOption.value());
+    }
+    else
+    {
+        cLog::getInstance()->init(logPriority, "");
+    }
 
+    INFO << ("Parsed and handled options\n");
+    WARNING << "Started log with priority: " << logPriority << "\n";
+    ERROR << "Test Error \n";
+    DEBUG << "Test Debug \n";
+ 
     //Pass arguments to Verilated code
     contextp->commandArgs(argc, argv);
 
@@ -62,6 +98,9 @@ int main(int argc, char* argv[])
 
     //destroy testbench
     delete testbench;
+
+    // Close the log
+    cLog::getInstance()->close();
     
     //Completed succesfully
     return 0;
